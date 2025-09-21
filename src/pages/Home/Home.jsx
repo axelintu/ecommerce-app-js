f0ece293f29fca8c0ba1da8d3fec2800b686e208
axelintu <darklintumon@gmail.com>
2025-09-21 12:44:02 -0600

    Moved Home Images, and products JSONs to data folder.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import "./Home.css";
 import ProductCard from "../../components/ProductCard/ProductCard";
+import homeImages from "../../data/homeImages.json";
 
 export default function Home ({products}) {
   return (<div className="products-grid">
