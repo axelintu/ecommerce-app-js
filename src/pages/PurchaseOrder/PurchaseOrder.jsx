eea16f9b9304468d7d9292fbf2e38163c43c3537
axelintu <darklintumon@gmail.com>
2025-11-05 23:39:13 -0600

    Updated PurchaseOrder

--- src/pages/PurchaseOrder/PurchaseOrder.jsx
+++ src/pages/PurchaseOrder/PurchaseOrder.jsx
@@ -3,29 +3,76 @@ import CartView from '../../components/Cart/CartView/CartView';
 import Button from '../../components/common/Button';
 
 function PurchaseOrder() {
+  const addressList = [
+    {
+      name: "Work",
+      address1: "Calle 2",
+      address2: "Colonia 2",
+      postalCode: "20000",
+      city: "Aguascalientes",
+      country: "México",
+      reference: "Entre calle A y B",
+      default: true
+    }
+  ];
+  const paymentMethodList = [
+    {
+      alias: "Tarjeta1",
+      cardNumber: "4444-4444-4444-4444",
+      cardHolder: "Rodrigo",
+      expiryDate: "08/31",
+      cvv: "123",
+      default: true
+    },
+    {
+      alias: "Tarjeta2",
+      cardNumber: "4444-4444-4444-4444",
+      cardHolder: "Rodrigo",
+      expiryDate: "08/31",
+      cvv: "123",
+      default: false
+    }
+  ];
+
+
+
   return (
     <div className="order-container">
       <div className="order-left">
         <div className="order-address">
-          <p>{name} {adress}</p>
+          <p>
+            {addressList.find(a => a.default===true).name} 
+            {addressList.find(a => a.default===true).address1}
+          </p>
           <Button>Cambiar</Button>
           <div className="address-list">
             <ul>
-              <li>
-                <h3>Nombre</h3>
-                <p>Dirección</p>
-                <p>Telefono</p>
-              </li>
+              {
+                addressList.map(addss => {
+                  return (
+                    <li>
+                      <h3>{addss.name}</h3>
+                      <p> {addss.address1}</p>
+                      <p> {addss.address2}</p>
+                      <p> {addss.postalCode}</p>
+                      <p> {addss.city}</p>
+                      <p> {addss.reference}</p>
+                    </li>
+                  )
+                })
+              }
             </ul>
           </div>
           <form action="" className="address-form">
-            <label><div>Nombre: </div><Input label="{newName}" type="text"></Input></label>
+            <label><div>Nombre:    </div><Input label="{newName}" type="text">{}</Input></label>
             <label><div>Dirección: </div><Input label="{newAddress}" type="text"></Input></label>
-            <label><div>Teléfono: </div><Input label="{newPhone}" type="text"></Input></label>
+            <label><div>Teléfono:  </div><Input label="{newPhone}" type="text"></Input></label>
             <Button>Guardar</Button>
           </form>
         </div>
-        <div className="order-payment"></div>
+        <div className="order-payment">
+
+        </div>
         <CartView />
       </div>
       <div className="order-right">
