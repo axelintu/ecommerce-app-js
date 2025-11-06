35dbccc6e303c5f83332e00681a7bf12d9c9d842
axelintu <darklintumon@gmail.com>
2025-11-05 18:55:14 -0600

    Added SearchResults page

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from '../src/components/App';
 import reportWebVitals from './reportWebVitals';
-// import { ThemeProvider } from './context/ThemeContext';
+import { ThemeProvider } from './context/ThemeContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <ThemeProvider> */}
+    <ThemeProvider>
       <App />
-    {/* </ThemeProvider> */}
+    </ThemeProvider>
   </React.StrictMode>
 );
 
