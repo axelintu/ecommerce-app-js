cabf4cbe7cdd73cf67ef5c80adfff0a987875494
axelintu <darklintumon@gmail.com>
2025-11-05 20:29:24 -0600

    Added initial files for Cart process pages and functions

---
+++ src/components/Cart/CartView/CartView.jsx
@@ -0,0 +1,12 @@
+import { useCart } from "../../../context/cartContext";
+import './CartView.css';
+
+function CartView() {
+  return (
+    <div className="cart-view">
+      <h1>Estos son los productos en tu carrito</h1>
+    </div>
+  );
+}
+
+export default CartView;
