89128056c04c230c74fba3e2724099b2e4bbaff1
axelintu <darklintumon@gmail.com>
2025-11-05 19:45:48 -0600

    Updated App.jsx with ProtectedRoute

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,7 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Layout from "../../layout";
 import Home from "../../pages/Home";
-import SearchResults from "../../pages/SearchResults";
+import SearchResults from "../../pages/SearchResultsList";
+import ProtectedRoute from "../../pages/ProtectedRoute";
 
 function App() {
   return (
@@ -11,6 +12,15 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/search" element={<SearchResults />} />
+            <Route path="/profile"
+            element={
+              <ProtectedRoute 
+                redirectTo="/login" 
+                allowedRoles={["admin","customer"]}
+              >
+                <Profile />
+              </ProtectedRoute>
+            }/>
             <Route path="*" element={<div>Ruta no encontrada</div>} />
           </Routes>
         </Layout>
